8d16bc19212918c91ba89e9d7cf4ce4ea7236848
Harold Arriola Nucamendi <arriolaharold2001@gmail.com>
2022-04-27 10:00:00 -0400

    overloading fucntions examples

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,14 @@
+import javax.print.event.PrintEvent;
+
 public class Main {
   public static void main(String[] args){
-
-    UnipedalRobots robot1 = new UnipedalRobots("Harold");
-    BipedalRobots robot2 = new BipedalRobots("Perla");
-    QuadrupedalRobots robot3 = new QuadrupedalRobots("Milo");
-    ArachnidRobots robot4 = new ArachnidRobots("Carlee");
-    RadialRobots robot5 = new RadialRobots("Peter");
-    AeronauticalRobots robot6 = new AeronauticalRobots("Frank");
-
-    //ALL TYPES OF ROBOTS HAVE ACCESS TO THIS METHOD FROM ROBOT CLASS
-    robot1.cleanPlates();
-    robot2.cleanPlates();
-    robot3.cleanPlates();
-    robot4.cleanPlates();
-    robot5.cleanPlates();
-    robot6.cleanPlates();
-    
-    
-    //UNIQUE METHODS BASED ON TYPE OF ROBOT 
-    robot1.rideUnicycle();
-    robot2.rideBicycle();
-    robot3.rideFourWheeler();
-    robot4.makeSpider();
-    robot5.makeCircle();
-    robot6.flying();
-
-    
+    System.out.println(add(1,2,3));
+    System.out.println(add(1,2));
+  }
+  public static int add(int a, int b) {
+    return a + b;
+  }
+  public static int add(int a, int b, int c) {
+    return a + b + c;
   }
 }
